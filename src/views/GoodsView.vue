1dd6760118d0d4108aea1c82fdc38a91f559cc4a
DigMMA <tpdigmma@gmail.com>
2023-12-26 16:49:26 +0300

    My first task for Vue.js 3

---
+++ src/views/GoodsView.vue
@@ -0,0 +1,49 @@
+<script setup>
+import { goodsArray } from "../constants/goods";
+const getUrl = (name) => {
+  return new URL(`../assets/img/goods/${name}`, import.meta.url);
+};
+</script>
+
+<template>
+  <section class="products">
+    <div class="container">
+      <div class="products-wrapper" id="goods-container">
+        <!--<div class="products-card">-->
+        <router-link
+          to="/"
+          class="products-card"
+          v-for="goods in goodsArray"
+          :key="goods.id"
+        >
+          <div class="products-card__image">
+            <img :src="getUrl(goods.image)" alt="" />
+          </div>
+          <div class="products-card__description">
+            <div class="products-card__description-row">
+              <h5 class="products-card__description--name">
+                {{ goods.title }}
+              </h5>
+            </div>
+            <div class="products-card__description-row">
+              <p class="products-card__description--text">{{ goods.desc }}</p>
+            </div>
+            <div class="products-card__description-row">
+              <div class="products-card__description-controls">
+                <button class="btn btn-primary">
+                  В корзину<i
+                    class="fa fa-shopping-basket"
+                    aria-hidden="true"
+                  ></i>
+                </button>
+                <span class="products-card__description-controls--price"
+                  >{{ goods.price }} ₽</span
+                >
+              </div>
+            </div>
+          </div>
+        </router-link>
+      </div>
+    </div>
+  </section>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,67 @@
+<template>
+  <section class="banner">
+    <div class="container">
+      <div class="banner-item">
+        <h1 class="banner-item--title">Онлайн-сервис доставки еды на дом</h1>
+        <p class="banner-item--subtitle">
+          Блюда из любимого ресторана привезет курьер в перчатках, маске и с
+          антисептиком
+        </p>
+      </div>
+    </div>
+  </section>
+  <section class="products">
+    <div class="container">
+      <div class="products-header">
+        <h3 class="products-header--title">Рестораны</h3>
+        <input
+          class="products-header--search"
+          type="text"
+          placeholder="Поиск блюд и ресторанов"
+        />
+      </div>
+      <div class="products-wrapper" id="rests-container">
+        <router-link
+          :to="`./rest?id=${rest.id}`"
+          class="products-card"
+          v-for="rest in restArray"
+          :key="rest.id"
+        >
+          <div class="products-card__image">
+            <img :src="getUrl(rest.image)" alt="" />
+          </div>
+          <div class="products-card__description">
+            <div class="products-card__description-row">
+              <h4 class="products-card__description--title">
+                {{ rest.title }}
+              </h4>
+              <div class="products-card__description--badge">
+                {{ rest.time }} мин
+              </div>
+            </div>
+            <div class="products-card__description-row">
+              <div class="products-card__description-info">
+                <div class="products-card__description-info--rating">
+                  <img src="../assets/img/star.svg" alt="" />{{ rest.rating }}
+                </div>
+                <div class="products-card__description-info--price">
+                  От {{ rest.price }} ₽
+                </div>
+                <div class="products-card__description-info--group">
+                  {{ rest.type }}
+                </div>
+              </div>
+            </div>
+          </div>
+        </router-link>
+      </div>
+    </div>
+  </section>
+</template>
+<script setup>
+import { restArray } from "../constants/rests";
+const getUrl = (name) => {
+  return new URL(`${name}`, import.meta.url);
+  // return `..assets/img/rests/image-01.jpg`
+};
+</script>
